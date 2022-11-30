b599eb25550efaed755b264d92b648f5430621e0
LongingTT <longingtt@163.com>
2022-11-30 22:12:24 +0800

    9 files modified, 1 added

--- erlang-ui/src/app/app.component.ts
+++ erlang-ui/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'erlang-ui';
+  darkTheme: boolean = false;
+
+  onToggleDarkTheme(isDarkTheme: boolean) {
+    this.darkTheme = isDarkTheme;
+  }
 }
--- erlang-ui/src/app/core/header/header.component.ts
+++ erlang-ui/src/app/core/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @Output() toggleDarkTheme = new EventEmitter<boolean>();
 
+  isDarkTheme: boolean = false;
+  isSigned: boolean = false;
+
+  onChangeTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.toggleDarkTheme.emit(this.isDarkTheme);
+  }
 }
